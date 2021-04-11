390f9880ff4ba4d3e6db5435aa5a182cae0af92b
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-04-11 21:22:24 +0300

    add Board's UML

--- Backend/BusinessLayer/PasswordController.cs
+++ Backend/BusinessLayer/PasswordController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,24 @@ using System.Threading.Tasks;
 namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     class PasswordController
-
     {
+        private int MIN_LEN;
+        private int MAX_LEN;
+
+        public PasswordController(int min_len, int max_len)
+        {
+            this.MIN_LEN = min_len;
+            this.MAX_LEN = max_len;
+        }
+
+        public Response<bool> isValid(string password)
+        {
+            return null;
+        }
+
+        public Response<Password> createPassword(string password)
+        {
+            return null;
+        } 
     }
 }
