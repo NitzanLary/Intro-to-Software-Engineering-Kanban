dd1d2844d7747b09688988a9aa102f7c263b7c0d
Yanay <yanayshem@gmail.com>
2021-04-11 21:23:07 +0300

    task and taskController functions signetures

--- Backend/BusinessLayer/Task.cs
+++ Backend/BusinessLayer/Task.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,57 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     class Task
     {
+        private readonly int ID;
+        private DateTime creationTime;
+        public DateTime CreationTime
+        {
+            get => creationTime;
+            set => creationTime = value;
+
+        }
+        private DateTime dueDate;
+        public DateTime DueDate
+        {
+            get => dueDate;
+            set => dueDate = value;
+        }
+        private string title;
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        private string description;
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        } 
+
+        Task(int ID, DateTime creationTime, string title, string description)
+        {
+            this.ID = ID;
+            this.creationTime = creationTime;
+            this.title = title;
+            this.description = description;
+        }
+
+        
+
+        public Response UpdateTaskDueDate(DateTime newDueDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateTaskTitle(string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateTaskDescription(string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- Backend/BusinessLayer/TaskController.cs
+++ Backend/BusinessLayer/TaskController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     class TaskController
     {
+        readonly private int MAX_TITLE = 50;
+        readonly private int MAX_DESC = 300;
+
+  
+
+        public Response AddTask(string title, string description, DateTime dueDate, int taskID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateTaskDueDate(Task task, DateTime NewDueDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateTaskTitle(Task task, string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response UpdateTaskDescription(Task task, string description)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
