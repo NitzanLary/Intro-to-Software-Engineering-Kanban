ad56679fa53a8c2504a9a9a9bb79c4308e195e95
Yanay <yanayshem@gmail.com>
2021-04-13 16:32:49 +0300

    - Runnable Test project - Logger created in User at service

--- Backend/ServiceLayer/Objects/User.cs
+++ Backend/ServiceLayer/Objects/User.cs
@@ -1,12 +1,26 @@
-﻿namespace IntroSE.Kanban.Backend.ServiceLayer
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Reflection;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer
 {
     public struct User
     {
         public readonly string Email;
 
+        //Logger
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+
         internal User(string email)
         {
             this.Email = email;
+
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            log.Info("Logger test!");
         }
     }
 }
