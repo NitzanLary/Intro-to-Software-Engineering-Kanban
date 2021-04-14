e334c13c3d4755a5b0e106b05f03618d922be458
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-04-14 16:43:10 +0300

    fix bugs

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,35 +10,32 @@ namespace Test
         static void Main(string[] args)
         {
             Service s = new Service();
-            Console.WriteLine(s.AddBoard("Rafa@gmail.com", "semster B").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa@gmail.com", "12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafagmail.com", "Rf12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa@gmail.com", "Rf12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa@gmail.com", "Rf12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "R12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "ff12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "RF12345678").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "Rf2").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "R12345678222222222222222222222222222222222f").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "R12345678ש").ErrorMessage);
-            Console.WriteLine(s.Register("Rafa111111@gmail.com", "R12345678f").ErrorMessage);
+           Print(s.AddBoard("Rafa@gmail.com", "semster B"));
+            Print(s.Register("Rafa@gmail.com", "12345678"));
+            Print(s.Register("Rafagmail.com", "Rf12345678"));
+            Print(s.Register("Rafa@gmail.com", "Rf12345678"));
+            Print(s.Register("Rafa@gmail.com", "Rf12345678"));
+            Print(s.Register("Rafa111111@gmail.com", "R12345678"));
+            Print(s.Register("Rafa111111@gmail.com", "ff12345678"));
+            Print(s.Register("Rafa111111@gmail.com", "RF12345678"));
+            Print(s.Register("Rafa111111@gmail.com", "Rf2"));
+            Print(s.Register("Rafa111111@gmail.com", "R12345678222222222222222222222222222222222f"));
+            Print(s.Register("Rafa111111@gmail.com", "R12345678ש"));
+            Print(s.Register("Rafa111111@gmail.com", "R12345678f"));
             //Console.WriteLine(s.AddBoard("Rada@gmail.com", "Semester B").ErrorMessage);
             //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
             //Console.WriteLine(s.Login("Rafa@gmail.com", "Rf12345678").ErrorMessage);
             //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
 
 
-
-
-
-
-        static void print(Response res)
+        }
+        static void Print(Response res)
         {
             string input = "";
             if (res.ErrorOccured)
                 input = res.ErrorMessage;
             else
-                input = "no error  "+ res.GetType().Name;
+                input = "no error  " + res.GetType().Name;
             Console.WriteLine($"\n    -  ----------   {input}   ----------   -\n");
         }
     }
