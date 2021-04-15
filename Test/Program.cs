bf820120c8fa5583c474ffbe2d1700c23cd676b2
Nitzan <laryn@post.bgu.ac.il>
2021-04-15 19:36:24 +0300

    User: - erasing un neccesary logger line Program: - more tests

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,86 +10,89 @@ namespace Test
         static void Main(string[] args)
         {
             Service s = new Service();
-           //Print(s.AddBoard("Rafa@gmail.com", "semster B"));
-            //Print(s.Register("Rafa@gmail.com", "12345678"));
-            //Print(s.Register("Rafagmail.com", "Rf12345678"));
-            Print(s.Register("Rafa@gmail.com", "Rf12345678"));
-            //Print(s.Register("Rafa@gmail.com", "Rf12345678"));
-            //Print(s.Register("Rafa111111@gmail.com", "R12345678"));
-            //Print(s.Register("Rafa111111@gmail.com", "ff12345678"));
-            //Print(s.Register("Rafa111111@gmail.com", "RF12345678"));
-            //Print(s.Register("Rafa111111@gmail.com", "Rf2"));
-            //Print(s.Register("Rafa111111@gmail.com", "R12345678222222222222222222222222222222222f"));
-            //Print(s.Register("Rafa111111@gmail.com", "R12345678ש"));
-            Print(s.Register("Rafa111111@gmail.com", "R12345678f"));
-            //Print(s.Login("Rafa@gmail.com", "R12345678f"));
-            Print(s.AddBoard("Rafa@gmail.com", "semester B"));
-            Print(s.Login("Rafa@gmail.com", "Rf12345678"));
-            //Print(s.Login("Rafa123@gmail.com", "Rf12345678"));
-            Print(s.Login("Rafa111111@gmail.com", "R12345678f"));
-            //Print(s.Logout("Rafa@gmailcom"));
-            //Print(s.Logout("Rafa@gmail.com"));
+            init(s);
+            Task(s);
 
-            //Print(s.Logout("Rafagmail.com"));
-            //Print(s.Logout("Rada@gmail.com"));
-            //Print(s.Logout("Rafa111111@gmail.com"));
+           ////Print(s.AddBoard("Rafa@gmail.com", "semster B"));
+           // //Print(s.Register("Rafa@gmail.com", "12345678"));
+           // //Print(s.Register("Rafagmail.com", "Rf12345678"));
+           // Print(s.Register("Rafa@gmail.com", "Rf12345678"));
+           // //Print(s.Register("Rafa@gmail.com", "Rf12345678"));
+           // //Print(s.Register("Rafa111111@gmail.com", "R12345678"));
+           // //Print(s.Register("Rafa111111@gmail.com", "ff12345678"));
+           // //Print(s.Register("Rafa111111@gmail.com", "RF12345678"));
+           // //Print(s.Register("Rafa111111@gmail.com", "Rf2"));
+           // //Print(s.Register("Rafa111111@gmail.com", "R12345678222222222222222222222222222222222f"));
+           // //Print(s.Register("Rafa111111@gmail.com", "R12345678ש"));
+           // Print(s.Register("Rafa111111@gmail.com", "R12345678f"));
+           // //Print(s.Login("Rafa@gmail.com", "R12345678f"));
+           // Print(s.AddBoard("Rafa@gmail.com", "semester B"));
+           // Print(s.Login("Rafa@gmail.com", "Rf12345678"));
+           // //Print(s.Login("Rafa123@gmail.com", "Rf12345678"));
+           // Print(s.Login("Rafa111111@gmail.com", "R12345678f"));
+           // //Print(s.Logout("Rafa@gmailcom"));
+           // //Print(s.Logout("Rafa@gmail.com"));
 
-            // --------------------- AddBoard Tests -----------------------
-            Print(s.AddBoard("Rafa@gmail.com", "semester B"));
-            Print(s.AddBoard("Rafa@gmail.com", "semester A"));
-            Print(s.AddBoard("Rafa111111@gmail.com", "semester B"));
+           // //Print(s.Logout("Rafagmail.com"));
+           // //Print(s.Logout("Rada@gmail.com"));
+           // //Print(s.Logout("Rafa111111@gmail.com"));
 
-            // --------------------- AddTask Tests -----------------------
+           // // --------------------- AddBoard Tests -----------------------
+           // Print(s.AddBoard("Rafa@gmail.com", "semester B"));
+           // Print(s.AddBoard("Rafa@gmail.com", "semester A"));
+           // Print(s.AddBoard("Rafa111111@gmail.com", "semester B"));
 
-            Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
-            //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
-            Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "", new DateTime(2021, 5, 14, 0, 0, 0))); // possible the same name for task???
-            //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
-            Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 2", new DateTime(2021, 5, 14, 0, 0, 0)));
-            //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 2", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
-            Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 3", new DateTime(2021, 5, 14, 0, 0, 0))); 
-            Print(s.AddTask("Rafa@gmail.com", "semester A", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0))); 
-            Print(s.AddTask("Rafa111111@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
-            //Print(s.AddTask("a1@gmail.com", "semester B", "SE", "test", new DateTime(2021, 5, 14, 0, 0, 0)));
-            //Print(s.AddTask("Rafa111111@gmail.com", "semester C", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
+           // // --------------------- AddTask Tests -----------------------
 
-            // --------------------- AdvenceTask Tests -----------------------
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 10)); // EROR ID not exsit
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 1));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 2));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 1));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 1));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 2, 1));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 3));
-            //Print(s.AdvanceTask("Rafa@gmail.com", "semester A", 0, 5));
+           // Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
+           // //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
+           // Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "", new DateTime(2021, 5, 14, 0, 0, 0))); // possible the same name for task???
+           // //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
+           // Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 2", new DateTime(2021, 5, 14, 0, 0, 0)));
+           // //Console.WriteLine(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 2", new DateTime(2021, 5, 14, 0, 0, 0)).Value.Id);
+           // Print(s.AddTask("Rafa@gmail.com", "semester B", "SE", "task 3", new DateTime(2021, 5, 14, 0, 0, 0))); 
+           // Print(s.AddTask("Rafa@gmail.com", "semester A", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0))); 
+           // Print(s.AddTask("Rafa111111@gmail.com", "semester B", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
+           // //Print(s.AddTask("a1@gmail.com", "semester B", "SE", "test", new DateTime(2021, 5, 14, 0, 0, 0)));
+           // //Print(s.AddTask("Rafa111111@gmail.com", "semester C", "SE", "task 1", new DateTime(2021, 5, 14, 0, 0, 0)));
 
-
-            //// --------------------- UpdateTask Tests ----------------
-            ////Print(s.UpdateTaskDescription("Rafa@gmail.com", "semester B", 0, 2, "1.1"));
-            //////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 13, 14, 0, 0, 0))); // EROR invalid data 
-            ////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 4, 14, 0, 0, 0))); // EROR passed date
-            ////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 6, 15, 0, 0, 0)));
-            ////Print(s.UpdateTaskTitle("Rafa@gmail.com", "semester B", 0, 2, "")); // EROR canot be empty title
-            ////Print(s.UpdateTaskTitle("Rafa@gmail.com", "semester B", 0, 2, "semseter C"));
-
-            //// --------------------- InProgress Tests ----------------
-
-            //Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[0].Id);
-            //Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[1].Id);
-            ////Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[2].Id); // EROR 
+           // // --------------------- AdvenceTask Tests -----------------------
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 10)); // EROR ID not exsit
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 1));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 2));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 1));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 1, 1));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 2, 1));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester B", 0, 3));
+           // //Print(s.AdvanceTask("Rafa@gmail.com", "semester A", 0, 5));
 
 
+           // //// --------------------- UpdateTask Tests ----------------
+           // ////Print(s.UpdateTaskDescription("Rafa@gmail.com", "semester B", 0, 2, "1.1"));
+           // //////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 13, 14, 0, 0, 0))); // EROR invalid data 
+           // ////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 4, 14, 0, 0, 0))); // EROR passed date
+           // ////Print(s.UpdateTaskDueDate("Rafa@gmail.com", "semester B", 0, 2, new DateTime(2021, 6, 15, 0, 0, 0)));
+           // ////Print(s.UpdateTaskTitle("Rafa@gmail.com", "semester B", 0, 2, "")); // EROR canot be empty title
+           // ////Print(s.UpdateTaskTitle("Rafa@gmail.com", "semester B", 0, 2, "semseter C"));
 
+           // //// --------------------- InProgress Tests ----------------
 
-
+           // //Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[0].Id);
+           // //Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[1].Id);
+           // ////Console.WriteLine(s.InProgressTasks("Rafa@gmail.com").Value[2].Id); // EROR 
 
 
 
 
-            //Console.WriteLine(s.AddBoard("Rada@gmail.com", "Semester B").ErrorMessage);
-            //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
-            //Console.WriteLine(s.Login("Rafa@gmail.com", "Rf12345678").ErrorMessage);
-            //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
+
+
+
+
+
+           // //Console.WriteLine(s.AddBoard("Rada@gmail.com", "Semester B").ErrorMessage);
+           // //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
+           // //Console.WriteLine(s.Login("Rafa@gmail.com", "Rf12345678").ErrorMessage);
+           // //Console.WriteLine(s.AddBoard("Rafa@gmail.com", "Semester B").ErrorMessage);
 
 
         }
@@ -116,6 +119,20 @@ namespace Test
 
         static void Task(Service s)
         {
+            DateTime date = new DateTime(2021, 5, 16, 0, 0, 0);
+            string mail = "Rafa@gmail.com";
+            validation(s.AddBoard("Rafa@gmail.com", null), true);
+            validation(s.AddBoard("Rafa@gmail.com", "myBoard"), false);
+            validation(s.AddBoard("Rafa@gmail.com", "myBoard"), true);
+            validation(s.AddTask(mail, null, "myTitle", "myDesc", date), true);
+            validation(s.AddTask(mail, "myBoard", null, "myDesc", date), true);
+            validation(s.AddTask(mail, "myBoard", "myTitle", null, date), true);
+            validation(s.AddTask(mail, "myBoard", "myTitle1", "myDesc1", date), false);
+            validation(s.AddTask(mail, "myBoard", "myTitle2", "myDesc2", date), false);
+            Response<Task> rT1 = s.AddTask(mail, "myBoard", "myTitle1", "myDesc1", date);
+            Response<Task> rT2 = s.AddTask(mail, "myBoard", "myTitle2", "myDesc2", date);
+            
+
 
         }
 
