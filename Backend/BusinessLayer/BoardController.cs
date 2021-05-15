7eec1f8e775c965a82cb0a1917b5dd691d421a06
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-05-15 19:01:44 +0300

    convert BoardController to new M2

--- Backend/BusinessLayer/BoardController.cs
+++ Backend/BusinessLayer/BoardController.cs
@@ -32,7 +32,9 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="email">The email address of the user, must be logged in</param>
         /// <param name="boardName">The name of the board</param>
         /// <returns>A response object. The response should contain a error message in case of missing board for user or invalid argments</returns>
-        private Response AllBoardsContainsBoardByEmail(string email, string boardName) // todo - valid input checker - add to diagram
+        
+        // TODO: check if creatorEmail is valid too
+        private Response AllBoardsContainsBoardByEmail(string userEmail, string creatorEmail, string boardName) 
         {
             if (email == null || boardName == null || email.Length == 0 || boardName.Length == 0)
                 return new Response("null value given");
@@ -52,14 +54,15 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="limit">The new limit value. A value of -1 indicates no limit.</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response LimitColumn(string email, string boardName, int columnOrdinal, int limit) 
+        public Response LimitColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int limit) 
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
                 return validArguments;
             if (columnOrdinal > 2)
                 return new Response("column ordinal dose not exist. max 2");
-            return boards[email][boardName].limitColumn(columnOrdinal, limit);
+            // TODO: creatorEmail is userEmail  
+            return boards[userEmail][boardName].limitColumn(columnOrdinal, limit);
         }
         /// <summary>
         /// Get the limit of a specific column
@@ -68,14 +71,15 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>The limit of the column.</returns>
-        public Response<int> GetColumnLimit(string email, string boardName, int columnOrdinal)
+        public Response<int> GetColumnLimit(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
                 return Response<int>.FromError(validArguments.ErrorMessage);
             if (columnOrdinal > 2)
                 return Response<int>.FromError("column ordinal dose not exist. max 2");
-            return boards[email][boardName].getColumnLimit(columnOrdinal);
+
+            return boards[userEmail][boardName].getColumnLimit(columnOrdinal);
         }
         /// <summary>
         /// Get the name of a specific column
@@ -84,9 +88,9 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>The name of the column.</returns>
-        public Response<string> GetColumnName(string email, string boardName, int columnOrdinal) 
+        public Response<string> GetColumnName(string userEmail, string creatorEmail, string boardName, int columnOrdinal) 
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
             {
                 log.Debug(validArguments.ErrorMessage);
@@ -94,7 +98,7 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             }
             if (columnOrdinal > 2)
                 return Response<string>.FromError("column ordinal dose not exist. max 2");
-            return boards[email][boardName].getColumnName(columnOrdinal);
+            return boards[userEmail][boardName].getColumnName(columnOrdinal);
         }
         /// <summary>
         /// Add a new task.
@@ -105,14 +109,16 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="description">Description of the new task</param>
         /// <param name="dueDate">The due date if the new task</param>
         /// <returns>A response object with a value set to the Task, instead the response should contain a error message in case of an error</returns>
-        public Response<Task> AddTask(string email, string boardName, string title, string description, DateTime dueDate)
+        
+        // TODO: CHANGE !
+        public Response<Task> AddTask(string userEmail, string creatorEmail, string boardName, string title, string description, DateTime dueDate)
         {
             if (title == null || title.Length == 0) // || description == null
                 return Response<Task>.FromError("empty string given");
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
                 return Response<Task>.FromError(validArguments.ErrorMessage);
-            Board b = boards[email][boardName];
+            Board b = boards[userEmail][boardName];
             Response<Task> r = taskController.AddTask(title, description, dueDate);
             if (r.ErrorOccured)
             {
@@ -156,9 +162,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="dueDate">The new due date of the column</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskDueDate(string email, string boardName, int columnOrdinal, int taskId, DateTime dueDate) 
+        
+        
+        public Response UpdateTaskDueDate(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, DateTime dueDate) 
         {
-            Response<Task> res = TaskGetter(email, boardName, columnOrdinal, taskId);
+            // TODO: check only assignee
+            Response<Task> res = TaskGetter(userEmail, boardName, columnOrdinal, taskId);
             if (res.ErrorOccured)
             {
                 log.Error(res.ErrorMessage);
@@ -177,8 +186,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="title">New title for the task</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskTitle(string email, string boardName, int columnOrdinal, int taskId, string title) 
+        
+        
+        public Response UpdateTaskTitle(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string title) 
         {
+            // TODO: check only assignee
+
             if (title == null || title.Length == 0)
                 return Response<Task>.FromError("empty string given");
             Response<Task> res = TaskGetter(email, boardName, columnOrdinal, taskId);
@@ -200,8 +213,10 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <param name="description">New description for the task</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response UpdateTaskDescription(string email, string boardName, int columnOrdinal, int taskId, string description) 
+        public Response UpdateTaskDescription(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string description) 
         {
+            // TODO: check only assignee
+
             if (description == null || description.Length == 0)
                 return Response<Task>.FromError("empty string given");
             Response<Task> res = TaskGetter(email, boardName, columnOrdinal, taskId);
@@ -223,9 +238,11 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <param name="taskId">The task to be updated identified task ID</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response AdvanceTask(string email, string boardName, int columnOrdinal, int taskId) 
+        
+        
+        public Response AdvanceTask(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId) 
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
             {
                 log.Debug(validArguments.ErrorMessage);
@@ -243,12 +260,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="boardName">The name of the board</param>
         /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
         /// <returns>A response object with a value set to the Column, The response should contain a error message in case of an error</returns>
-        public Response<IList<Task>> GetColumn(string email, string boardName, int columnOrdinal)
+        public Response<IList<Task>> GetColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, boardName);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, boardName);
             if (validArguments.ErrorOccured)
                 return Response<IList<Task>>.FromError(validArguments.ErrorMessage);
-            Board b = boards[email][boardName];
+            Board b = boards[userEmail][boardName];
             Response<Dictionary<int, Task>> res = b.getColumn(columnOrdinal);
             if (res.ErrorOccured)
                 return Response<IList<Task>>.FromError(res.ErrorMessage);
@@ -277,16 +294,16 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// <param name="email">Email of the user. Must be logged in</param>
         /// <param name="name">The name of the board</param>
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
-        public Response RemoveBoard(string email, string name) 
+        public Response RemoveBoard(string userEmail, string creatorEmail, string name) 
         {
-            Response validArguments = AllBoardsContainsBoardByEmail(email, name);
+            Response validArguments = AllBoardsContainsBoardByEmail(userEmail, name);
             if (validArguments.ErrorOccured)
             {
                 log.Debug(validArguments.ErrorMessage);
                 return Response<Task>.FromError(validArguments.ErrorMessage);
             }
                 
-            boards[email].Remove(name);
+            boards[userEmail].Remove(name);
             return new Response();
             
         }
