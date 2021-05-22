790ef1612cb5fd598f5a40598bf48b03e90b1c89
Nitzan <laryn@post.bgu.ac.il>
2021-05-22 21:48:38 +0300

    delete insertNewUser from userDal

--- Backend/DataAccessLayer/UserDALController.cs
+++ Backend/DataAccessLayer/UserDALController.cs
@@ -30,45 +30,6 @@ namespace IntroSE.Kanban.Backend.DataAccessLayer
             return result;
         }
 
-
-
-        public bool InsertNewUser(UserDTO user)
-        {
-
-            using (var connection = new SQLiteConnection(_connectionString))
-            {
-                int res = -1;
-                SQLiteCommand command = new SQLiteCommand(null, connection);
-                try
-                {
-                    connection.Open();
-                    command.CommandText = $"INSERT INTO {UsersTableName} ({UserDTO.EmailColumnName} ,{UserDTO.PasswordColumnName}) " +
-                        $"VALUES (@emailVal,@passwordVal);";
-
-                    SQLiteParameter emailParam = new SQLiteParameter(@"emailVal", user.Email);
-                    SQLiteParameter passwordParam = new SQLiteParameter(@"passwordVal", user.Password);
-
-                    command.Parameters.Add(emailParam);
-                    command.Parameters.Add(passwordParam);
-                    command.Prepare();
-
-                    res = command.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    //log error
-                    log.Error(e.Message);
-                    throw;
-                }
-                finally
-                {
-                    command.Dispose();
-                    connection.Close();
-                }
-                return res > 0;
-            }
-        }
-
         protected override DTO ConvertReaderToObject(SQLiteDataReader reader)
         {
             return new UserDTO(reader.GetString(0), reader.GetString(1));
