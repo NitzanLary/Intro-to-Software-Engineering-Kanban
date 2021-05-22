885daf060af57a414ba0029107a1fecb4c53e0bb
Nitzan <laryn@post.bgu.ac.il>
2021-05-22 19:23:44 +0300

    creating Board and Column objects in SL

---
+++ Backend/ServiceLayer/Objects/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer.Objects
+{
+    public struct Board
+    {
+        public readonly string name;
+        public readonly string creator;
+        public List<Column> columns;
+        internal Board(string name, string creator, List<Column> columns)
+        {
+            this.name = name;
+            this.creator = creator;
+            this.columns = columns;
+        }
+    }
+}
