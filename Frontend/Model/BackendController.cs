911a36d8ebc05ee66429156e1458276d71c4ca78
Yanay <yanayshem@gmail.com>
2021-05-27 07:18:44 +0300

    - added object and bg image - Main window design

---
+++ Frontend/Model/BackendController.cs
@@ -0,0 +1,22 @@
+﻿using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Model
+{
+    class BackendController
+    {
+        private Service Service { get; set; }
+
+        public BackendController(Service service)
+        {
+            this.Service = service;
+            Service.LoadData();
+        }
+
+
+    }
+}
--- Frontend/View/MainWindow.xaml.cs
+++ Frontend/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Frontend.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace Frontend
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainViewModel viewModel;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainViewModel();
+            this.viewModel = (MainViewModel)DataContext;
         }
     }
 }
