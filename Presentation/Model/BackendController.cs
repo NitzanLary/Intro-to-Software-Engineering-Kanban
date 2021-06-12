92db3c3987b2e6decaac0befb5895489f3f84191
RefaelFarjun <farjunr@post.bgu.ac.il>
2021-06-12 03:01:23 +0300

    impl BeckendController

--- Presentation/Model/BackendController.cs
+++ Presentation/Model/BackendController.cs
@@ -191,8 +191,60 @@ namespace Presentation.Model
             }
         }
 
+        internal void AssignTask(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int taskId, string emailAssignee)
+        {
+            Response res = Service.AssignTask(userEmail, creatorEmail, boardName, columnOrdinal, taskId, emailAssignee);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+        }
 
+        internal IList<String> GetBoardNames(string userEmail)
+        {
+            Response<IList<String>> res = Service.GetBoardNames(userEmail);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+            return res.Value;
+        }
+        
+        internal void AddColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal, string columnName)
+        {
+            Response res = Service.AddColumn(userEmail, creatorEmail, boardName, columnOrdinal, columnName);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+        }
 
+        internal void RemoveColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal)
+        {
+            Response res = Service.RemoveColumn(userEmail, creatorEmail, boardName, columnOrdinal);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+        }
+
+        internal void RenameColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal, string newColumnName)
+        {
+            Response res = Service.RenameColumn(userEmail, creatorEmail, boardName, columnOrdinal, newColumnName);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+        }
+
+        internal void MoveColumn(string userEmail, string creatorEmail, string boardName, int columnOrdinal, int shiftSize)
+        {
+            Response res = Service.MoveColumn(userEmail, creatorEmail, boardName, columnOrdinal, shiftSize);
+            if (res.ErrorOccured)
+            {
+                throw new Exception(res.ErrorMessage);
+            }
+        }
 
     }
 }
